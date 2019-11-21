7a06538afde685395f07c5fab643432b8bf4f36d
Oscar Ligthart <oscarligthart@gmail.com>
2019-11-21 13:11:10 +0100

    removing excessive file

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pickle
-
-vocab =pickle.load( open( "data/dict_3.pckl", "rb" ) )
-
-print(vocab)
